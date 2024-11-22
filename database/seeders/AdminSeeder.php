289a118b9bbd694a45ffb5e6f2d88a8952328439
alimaroof <ali7maroof77@gmail.com>
2024-11-22 17:45:51 +0300

    create AdminSeeder, redirect to /dashboard

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use function Laravel\Prompts\table;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+            
+        DB::table('users')->insert([
+
+            'name'=>'admin',
+            'email'=>'admin@gmail.com',
+            'password'=>bcrypt('12345678'),
+
+        ]);
+
+    }
+}
