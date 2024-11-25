7d9803ec101b117a2250bce907f5434efeaaad0a
BsHeR4 <bshermahayni@gmail.com>
2024-11-25 15:54:47 +0300

    Setup create and store methods in UserController for create user operation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -20,14 +22,29 @@ class UserController extends Controller
     public function create()
     {
         //
+        return view('dashboard.manager.members.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
         //
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        // Add flash message
+        session()->flash('success', 'User created successfully.');
+        // Redirect based on the value of redirect_to
+        if ($request->redirect_to == 'index') {
+            return redirect()->route('users.index');
+        } else {
+            return redirect()->route('users.create');
+        }
     }
 
     /**
