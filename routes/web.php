b874152d93171d72e255025e0643b76b12930737
BsHeR4 <bshermahayni@gmail.com>
2024-11-26 19:52:33 +0300

    add forceDelete route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::resource('users', UserController::class);
-
+Route::delete('users/{id}/forceDelete', [UserController::class, 'forceDelete'])->name('users.forceDelete');
 Route::get('/', function () {
     return view('auth.login');
 });
