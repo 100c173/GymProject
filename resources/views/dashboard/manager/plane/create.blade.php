4e0c382d78854808ebbff87e8792276a2e03b458
BsHeR4 <bshermahayni@gmail.com>
2024-11-29 20:04:45 +0300

    Edit on create.blade.php for plan

--- resources/views/dashboard/manager/plane/create.blade.php
+++ resources/views/dashboard/manager/plane/create.blade.php
@@ -4,7 +4,7 @@
     <div class="row justify-content-center">
         <div class="col-lg-5">
             <div class="card shadow-lg border-0 rounded-lg mt-5">
-                <div class="card-header"><h3 class="text-center font-weight-light my-4">Create New Session</h3></div>
+                <div class="card-header"><h3 class="text-center font-weight-light my-4">Create New Plan</h3></div>
                 <div class="card-body">
                     <form  action="{{route('plans.store')}}" method="POST" enctype="multipart/form-data">
                         @csrf
@@ -21,26 +21,6 @@
                             <input name="price" class="form-control" id="inputPrice" type="number" placeholder="plan price">
                             <label for="inputPrice">Plane Price</label>
                         </div>
-                        <div>
-                            
-                            <label>
-                                <input type="radio" name="with_trainer"value="0"  required>
-                                (None) 
-                            </label>
-                        </div>
-                        <div>
-
-                            <label>
-                                <input type="radio" name="with_trainer"value="1"  required>
-                                (Personal Trainer) 
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                <input type="radio" name="with_trainer"value="2"  required>
-                                (Group)
-                            </label>
-                        </div>
 
                         <div class="form-floating mb-3">
                             <input name="period" class="form-control" id="inputPeriod" type="number" placeholder="Period">
@@ -48,18 +28,31 @@
                         </div>
 
                         <div class="form-floating mb-3">
-                           <select name="plan_type_id" class="status-dropdown">
-                            @foreach($plans as $plantype)
-                                <option name="plan_type_id" value="{{$plantype->id}}">{{$plantype->name}}</option>
-                             @endforeach  
-                           </select>
-
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                           
-                            <button type="submit" class="btn btn-primary" style="margin-top: 10px;">Save</button>
+                            <select id="planType" name="plan_type_id" class="form-control">
+                                @foreach($plans as $plantype)
+                                    <option name="plan_type_id" value="{{$plantype->id}}">{{$plantype->name}}</option>
+                                 @endforeach  
+                               </select>
+                               <label for="planType">Plan Type</label>
                         </div>
                         
+                        <div>
+                            
+                            <label>
+                                <input type="radio" name="with_trainer"value="1"  required>
+                                With Trainer
+                            </label>
+                        </div>
+                        <div>
+                            
+                            <label>
+                                <input type="radio" name="with_trainer"value="0"  required>
+                                Without Trainer
+                            </label>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-end mt-4 mb-0">
+                            <button type="submit" class="btn btn-primary" style="margin-top: 10px;">Create</button>
+                        </div>
                     </form>
                 </div>
                
