8ad0752084d2a8fd42c0e80a46d501daf7e72726
BsHeR4 <bshermahayni@gmail.com>
2024-11-29 20:05:11 +0300

    Edit on edit.blade.php for plan

--- resources/views/dashboard/manager/plane/edit.blade.php
+++ resources/views/dashboard/manager/plane/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="row justify-content-center">
         <div class="col-lg-5">
             <div class="card shadow-lg border-0 rounded-lg mt-5">
-                <div class="card-header"><h3 class="text-center font-weight-light my-4">Create New Session</h3></div>
+                <div class="card-header"><h3 class="text-center font-weight-light my-4">Edit Plan</h3></div>
                 <div class="card-body">
                     <form  action="{{route('plans.update',$plan)}}" method="POST" enctype="multipart/form-data">
                         @csrf
@@ -22,43 +22,37 @@
                             <input name="price"value="{{$plan->price}}" class="form-control" id="inputPrice" type="number" placeholder="plan price">
                             <label for="inputPrice">Plane Price</label>
                         </div>
-                        <label >
-                            {{$plan->with_trainer==0?"None":(($plan->with_trainer==1)?"Personal trainer":"Group")}}
-                        </label>
-                        <div>
-                            
-                            <label>
-                                <input type="radio" name="with_trainer"value="0"  required>
-                                (None) 
-                            </label>
-                        </div>
-                        <div>
-
-                            <label>
-                                <input type="radio" name="with_trainer"value="1"  required>
-                                (Personal Trainer) 
-                            </label>
-                        </div>
-                        <div>
-                            <label>
-                                <input type="radio" name="with_trainer"value="2"  required>
-                                (Group)
-                            </label>
-                        </div>
+                       
                         <div class="form-floating mb-3">
                             <input name="period"value="{{$plan->period}}" class="form-control" id="inputPeriod" type="number" placeholder="Period">
                             <label for="inputPeriod">Plane Period</label>
                         </div>
 
                         <div class="form-floating mb-3">
-                            <select name="plan_type_id" class="status-dropdown">
+                            <select id="planType" name="plan_type_id" class="form-control">
                                 @foreach($plans as $plantype)
-                                    <option name="plan_type_id" value="{{$plantype->id}}">{{$plantype->name}}</option>
+                                    <option {{ $plan->plan_type_id == $plantype->id ? 'selected' : '' }} name="plan_type_id" value="{{$plantype->id}}">{{$plantype->name}}</option>
                                  @endforeach  
                                </select>
-
+                               <label for="planType">Plan Type</label>
                         </div>
-                        <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+
+                        <div>
+                            
+                            <label>
+                                <input {{ $plan->with_trainer == 1 ? 'checked' : '' }} type="radio" name="with_trainer"value="1"  required>
+                                With Trainer
+                            </label>
+                        </div>
+                        <div>
+                            
+                            <label>
+                                <input {{ $plan->with_trainer == 0 ? 'checked' : '' }} type="radio" name="with_trainer"value="0"  required>
+                                Without Trainer
+                            </label>
+                        </div>
+
+                        <div class="d-flex align-items-center justify-content-end mt-4 mb-0">
                             <button type="submit" class="btn btn-primary" style="margin-top: 10px;">Update</button>
                             
                         </div>
