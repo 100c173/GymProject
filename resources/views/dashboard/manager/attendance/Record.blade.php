c39d94ac45bd4088a3c4723313717395b2d290db
BsHeR4 <bshermahayni@gmail.com>
2024-11-30 13:19:19 +0300

    Fix conflicts

--- resources/views/dashboard/manager/attendance/Record.blade.php
+++ resources/views/dashboard/manager/attendance/Record.blade.php
@@ -1,58 +1,5 @@
 @extends('/dashboard/manager/layout')
 @section('content')
-<<<<<<< HEAD
-<div class="card mb-4">
-    <div class="card-header">
-        <i class="fas fa-table me-1"></i>
-        Attendance Record
-    </div>
-    <div class="card-body">
-        <table id="datatablesSimple">
-            <thead>
-                <tr>
-                    <tr>
-                        <th> Member Name</th>
-                        <th>Session Name </th>
-                        <th>Trainer </th>
-                        <th>Session Date </th>
-                        <th>Session start </th>
-                       <th>Attendence status</th>
-                        
-                    </tr>   
-                </tr>
-            </thead>
-            <tfoot>
-                <tr>
-                    <th>Member Name</th>
-                    <th>Session Name </th>
-                    <th>Trainer </th>
-                    <th>Session Date </th>
-                    <th>Session start </th>
-                   <th>Attendence status</th>
-                
-                </tr>
-            </tfoot>
-            <tbody>
-                @foreach($appointments as $appointment)
-                <tr>
-                    <td>{{$appointment->user->name}}</td>
-                    <td>{{$appointment->session->name}}</td>
-                    <td>{{$appointment->session->user->name}}</td>
-                    <td>{{$appointment->session->times->first()->day}} </td>
-                    <td>{{$appointment->session->times->first()->start_time}} </td>
-                   
-                    <td>  
-                        <select class="form-select" aria-label="حالة الحضور">
-                        <option value="present">Present</option>
-                        <option value="absent">Absent</option>
-                        <option value="late">Late</option>
-                        </select>
-                </tr>
-                @endforeach
-              
-            </tbody>
-        </table>
-=======
 
 @if(session('success'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -180,7 +127,6 @@
                 </div>
             </div>
         </div>
->>>>>>> feature/attendances
     </div>
 </div>
 
