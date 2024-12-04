a8bbcf6ee2ab549c84e36586e073ec5edec1dde3
BsHeR4 <bshermahayni@gmail.com>
2024-12-04 09:17:14 +0300

    Setup RatingController.php

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -2,16 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rating;
+use App\Services\RatingService;
 use Illuminate\Http\Request;
 
 class RatingController extends Controller
 {
+
+    /**
+     * Service to handle rating-related logic 
+     * and separating it from the controller
+     * 
+     * @var RatingService
+     */
+    protected $ratingService;
+
+    /**
+     * RatingController constructor
+     *
+     * @param RatingService $ratingService
+     */
+    public function __construct(RatingService $ratingService)
+    {
+        // Apply the auth middleware to ensure the user is authenticated
+        $this->middleware(['auth']);
+
+        // Inject the RatingService to handle rating-related logic
+        $this->ratingService = $ratingService;
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         //
+       $ratings = $this->ratingService->getAllRatingsAfterFilttering($request);
+        return view('new-dashboard.rating.list_rating', [
+            'ratings' => $ratings
+        ]);
     }
 
     /**
