a9e2e9db18c5a5cdb42e36c3cde8ddded2e00457
BsHeR4 <bshermahayni@gmail.com>
2024-12-04 09:29:05 +0300

    Setup service controller

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,16 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ServiceRequest;
+use App\Models\Service;
+use App\Services\ServiceService;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
+
+    /**
+     * Service to handle service-related logic 
+     * and separating it from the controller
+     * 
+     * @var ServiceService
+     */
+    protected $serviceService;
+
+    /**
+     * ServiceController constructor
+     *
+     * @param ServiceService $serviceService
+     */
+    public function __construct(ServiceService $serviceService)
+    {
+        // Apply the auth middleware to ensure the user is authenticated
+        $this->middleware(['auth']);
+
+        // Inject the ServiceService to handle service-related logic
+        $this->serviceService = $serviceService;
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         //
+        $services = $this->serviceService->getAllServicesAfterFilttering($request);
+        return view('new-dashboard.service.list_services', [
+            'services' => $services,
+        ]);
     }
 
     /**
@@ -20,20 +50,27 @@ class ServiceController extends Controller
     public function create()
     {
         //
+        return view('new-dashboard.service.create_service');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ServiceRequest $request)
     {
         //
+        $service = $this->serviceService->create($request);
+
+        // using the method from FlashMessageHelper to alert the user about success or faild
+        flashMessage($service, 'Service created successfully.', 'Failed to Create service.');
+        
+       return redirect()->route('services.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Service $service)
     {
         //
     }
@@ -41,24 +78,40 @@ class ServiceController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Service $service)
     {
         //
+        return view('new-dashboard.service.edit_service',[
+            'service' => $service,
+        ]);
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(ServiceRequest $request, Service $service)
     {
         //
+       $service = $this->serviceService->update($request, $service);
+
+        // using the method from FlashMessageHelper to alert the user about success or faild
+        flashMessage($service, 'Service updated successfully.', 'Failed to update service.');
+
+        return redirect()->route('services.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Service $service)
     {
         //
+       $service = $this->serviceService->delete($service);
+
+        // using the method from FlashMessageHelper to alert the user about success or faild
+        flashMessage($service, 'Service Deleted successfully.', 'Failed to Delete service.');
+
+        return redirect()->route('services.index');
     }
 }
