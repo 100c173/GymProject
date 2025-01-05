525e828e26bf190e7719c5e43dc79567e77c3c82
BsHeR4 <bshermahayni@gmail.com>
2025-01-05 17:02:14 +0300

    Modefiy RatingService.php For Api methods

--- app/Services/RatingService.php
+++ app/Services/RatingService.php
@@ -8,8 +8,51 @@ use Illuminate\Http\Request;
 class RatingService
 {
 
-    public function delete(Rating $rating)
+    /**
+     * To Create a new rating
+     * 
+     * @param array $data The create data
+     */
+    public function create(array $data)
     {
+        $rating = Rating::create([
+            'rating' => $data['rating'],
+            'comment' => $data['comment'],
+            'rateable_id' => $data['rateable_id'],
+            'rateable_type' => $data['rateable_type'],
+            'user_id' => $data['user_id'],
+        ]);
+
+        return $rating;
+    }
+
+    /**
+     * To Edit an rating
+     * 
+     * @param array $data The update data
+     * @param string $id The rating id to update
+     */
+    public function update(array $data, string $id)
+    {
+        $rating = Rating::findOrFail($id);
+
+        $rating->update($data);
+
+        return $rating;
+    }
+
+
+    public function show(string $id)
+    {
+        $rating = Rating::findOrFail($id);
+
+        return $rating;
+    }
+
+    public function delete(string $id)
+    {
+        $rating = Rating::findOrFail($id);
+
         return $rating->delete();
     }
 
@@ -20,41 +63,27 @@ class RatingService
      * It supports filtering by rating, rateable type, rater's full name, and rateable entity's name
      * The results are paginated based on the specified number of entries per page
      *
-     * @param Request $request The incoming request containing filter parameters
+     * @param array $data The incoming data containing filter parameters
      * @return LengthAwarePaginator The paginated list of filtered ratings
      */
-    public function getAllRatingsAfterFilttering(Request $request)
+    public function getAllRatingsAfterFiltering(array $data)
     {
-        // To define how many rows per page
-        $entries_number = $request->input('entries_number', 10);
+        $entries_number = $data['entries_number'] ?? 10;
 
-        $q = Rating::query();
+        $ratings = Rating::query()
+            ->when(isset($data['rating']) && $data['rating'] !== 'All', function ($query) use ($data) {
 
+                return $query->ofRating($data['rating']);
+            })->when(isset($data['rateable_type']) && $data['rateable_type'] !== '', function ($query) use ($data) {
 
-        // Apply filter for rating
-        if ($request->filled('rating') && $request->input('rating') !== 'All') {
-            $q->where('rating', $request->input('rating'));
-        }
+                return $query->ofType($data['rateable_type']);
+            })->when(isset($data['rater_name']), function ($query) use ($data) {
 
-        // Apply filter for rateable type
-        if ($request->filled('rateable_type') && $request->input('rateable_type') !== '') {
-            $q->where('rateable_type', $request->input('rateable_type'));
-        }
+                return $query->ofRaterName($data['rater_name']);
+            })->when(isset($data['rateable_name']), function ($query) use ($data) {
 
-        // Apply filter for rater full name
-        if ($request->filled('rater_name')) {
-            $q->whereHas('user', function ($query) use ($request) {
-                $query->SearchFullName($request->input('rater_name'));
-            });
-        }
-
-        // Apply filter for rateable name
-        if ($request->filled('rateable_name')) {
-            $q->SearchRateableName($request->input('rateable_name'));
-        }
-
-
-        $ratings = $q->paginate($entries_number);
+                return $query->ofRateableName($data['rateable_name']);
+            })->paginate($entries_number);
 
         return $ratings;
     }
