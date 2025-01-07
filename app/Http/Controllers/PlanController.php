d4e80537b1aaff11ba8d87e39376e76b32cbf6e0
BsHeR4 <bshermahayni@gmail.com>
2025-01-07 13:36:38 +0300

    Modify PlanController.php

--- app/Http/Controllers/PlanController.php
+++ app/Http/Controllers/PlanController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PlanFilterRequest;
 use App\Models\Plan;
 use App\Models\PlanType;
 use Illuminate\Http\Request;
 use App\Http\Requests\PlanRequest;
 use App\Services\PlanService;
+use App\Services\PlanTypeService;
 
 class PlanController extends Controller
 {
-
 
     /**
      * Service to handle plan-related logic 
@@ -21,29 +22,44 @@ class PlanController extends Controller
     protected $planService;
 
     /**
+     * Service to handle planType-related logic 
+     * and separating it from the controller
+     * 
+     * @var PlanTypeService
+     */
+    protected $planTypeService;
+
+    /**
      * PlanController constructor
      *
      * @param PlanService $planService
+     * @param PlanTypeService $planTypeService
      */
-    public function __construct(PlanService $planService)
+    public function __construct(PlanService $planService, PlanTypeService $planTypeService)
     {
         // Apply the auth middleware to ensure the user is authenticated
         $this->middleware(['auth']);
 
         // Inject the PlanService to handle plan-related logic
         $this->planService = $planService;
+
+        // Inject the PlanTypeService to handle planType-related logic
+        $this->planTypeService = $planTypeService;
     }
 
     /**
-     * Display a listing of the resource.
+     * Display a listing of the plans after applying filters
+     * 
+     * @param PlanFilterRequest $request The request object containing filter data 
+     * @return View The view displaying the list of plans
      */
-    public function index(Request $request)
+    public function index(PlanFilterRequest $request)
     {
-        $plan_types = PlanType::all();
+        $plan_types = $this->planTypeService->getAllPlanTypes();
+        $validated = $request->validated();
+        $plans = $this->planService->getAllPlansAfterFiltering($validated);
 
-        $plans = $this->planService->getAllPlansAfterFiltering($request);
-
-        return view('new-dashboard.plan.list_plans',[
+        return view('new-dashboard.plan.list_plans', [
             'plan_types' => $plan_types,
             'plans' => $plans
         ]);
@@ -55,7 +71,7 @@ class PlanController extends Controller
     public function create()
     {
         $plan_types = PlanType::all();
-        return view('new-dashboard.plan.create_plan',[
+        return view('new-dashboard.plan.create_plan', [
             'plan_types' => $plan_types
         ]);
     }
@@ -63,15 +79,19 @@ class PlanController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     * 
+     * @param PlanRequest $request To store the plan according to the conditions used
+     * in this form request
      */
     public function store(PlanRequest $request)
     {
-       $plan = $this->planService->create($request);
+        $validated = $request->validated();
+        $plan = $this->planService->create($validated);
 
         // using the method from FlashMessageHelper to alert the user about success or faild
         flashMessage($plan, 'Plan created successfully.', 'Failed to Create plan.');
 
-        return redirect()->route('plans.'. $request->redirect_to);
+        return redirect()->route('plans.' . $request->redirect_to);
     }
 
     /**
@@ -93,14 +113,18 @@ class PlanController extends Controller
 
     /**
      * Update the specified resource in storage.
+     * 
+     * @param PlanRequest $request To update the plan according to the conditions used
+     * in this form request
      */
-    public function update(Request $request, Plan $plan)
+    public function update(PlanRequest $request, Plan $plan)
     {
-        $plan = $this->planService->update($request,$plan);
+        $validated = $request->validated();
+        $plan = $this->planService->update($validated, $plan);
 
         // using the method from FlashMessageHelper to alert the user about success or faild
         flashMessage($plan, 'Plan updated successfully.', 'Failed to update plan.');
-        
+
         return redirect()->route('plans.index');
     }
 
@@ -109,12 +133,11 @@ class PlanController extends Controller
      */
     public function destroy(Plan $plan)
     {
-       $plan = $plan->delete();
+        $plan = $this->planService->delete($plan);
 
         // using the method from FlashMessageHelper to alert the user about success or faild
         flashMessage($plan, 'Plan Deleted successfully.', 'Failed to Delete plan.');
 
         return redirect()->route('plans.index');
     }
-
 }
