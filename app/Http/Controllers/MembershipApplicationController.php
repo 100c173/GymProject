8c271066d3ae0e7c982a734d624f33b8eda856f1
BsHeR4 <bshermahayni@gmail.com>
2025-01-09 00:38:45 +0300

    Modify MembershipApplicatoinController.php

--- app/Http/Controllers/MembershipApplicationController.php
+++ app/Http/Controllers/MembershipApplicationController.php
@@ -3,48 +3,67 @@
 namespace App\Http\Controllers;
 
 use App\Models\MembershipApplication;
+use App\Services\MembershipApplicationService;
 use Illuminate\Http\Request;
 
 class MembershipApplicationController extends Controller
 {
 
-    public function __construct()
+    /**
+     * Service to handle membershipApplication-related logic 
+     * and separating it from the controller
+     * 
+     * @var MembershipApplicationService
+     */
+    protected $membershipApplicationService;
+
+    /**
+     * MembershipApplicationController constructor
+     *
+     * @param MembershipApplicationService $membershipApplicationService
+     */
+    public function __construct(MembershipApplicationService $membershipApplicationService)
     {
         // Apply the auth middleware to ensure the user is authenticated
         $this->middleware(['auth']);
 
         // Apply the role middleware to ensure the user is admin
         $this->middleware(['role:admin'])->only('updateStatus', 'destroy');
+
+        // Inject the PermissionService to handle membershipApplication-related logic
+        $this->membershipApplicationService = $membershipApplicationService;
     }
+
     /**
-     * Display a listing of the resource.
+     * Display a listing of the permissions after applying filters
+     * 
+     * @param Request $request The request object containing filter data 
+     * @return View The view displaying the list of membership applications
      */
-    public function index()
+    public function index(Request $request)
     {
-        $memberships = MembershipApplication::with('user')->get();
+        $memberships = $this->membershipApplicationService
+            ->getAllMembershipApplicationsAfterFiltering($request->all());
+
         return view('new-dashboard.membership_application.list_membership_applications', compact('memberships'));
     }
 
 
     /**
-     * Update the specified resource in storage.
+     * Update the status of the specified membership application
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param string $id
+     * @return \Illuminate\Http\RedirectResponse 
      */
     public function updateStatus(Request $request, string $id)
     {
-        $application = MembershipApplication::findOrFail($id);
+        $status =  $this->membershipApplicationService->updateStatus($request->all(), $id);
 
+        // using the method from FlashMessageHelper to alert the user about success or faild
+        flashMessage($status, 'Status updated successfully.', 'Failed to Update status.');
 
-        if ($request->status === 'accept') {
-            $application->status = 'accepted';
-            ($application->user)->assignRole('trainer');
-        } else {
-            $application->status = 'declined';
-        }
-
-
-        $application->save();
-
-        return redirect()->back()->with('success', '');
+        return redirect()->back();
     }
 
     /**
@@ -52,7 +71,8 @@ class MembershipApplicationController extends Controller
      */
     public function show(string $id)
     {
-        $membership = MembershipApplication::findOrFail($id);
+        $membership = $this->membershipApplicationService->show($id);
+
         return view('new-dashboard.membership_application.show_membership_applications', [
             'membership' => $membership,
         ]);
@@ -63,8 +83,11 @@ class MembershipApplicationController extends Controller
      */
     public function destroy(string $id)
     {
-        $application = MembershipApplication::findOrFail($id);
-        $application->delete();
-        return redirect()->route('membership_applications')->with('success', '');
+        $membership = $this->membershipApplicationService->delete($id);
+
+        // using the method from FlashMessageHelper to alert the user about success or faild
+        flashMessage($membership, 'Membership application Deleted successfully.', 'Failed to Delete membership application.');
+
+        return redirect()->route('membership_applications');
     }
 }
