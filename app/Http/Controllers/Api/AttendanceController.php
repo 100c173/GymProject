b6e66ded29a787f974fdd752774bb00550304c92
C131-art <work46561@gmail.com>
2025-01-10 14:13:52 +0300

    AttendaceService

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -3,25 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Appointment;
-use Illuminate\Http\Request;
+use App\Services\AttendanceService;
 
 class AttendanceController extends Controller
 {
-    public function __construct()
+    protected $attendanceService;
+    public function __construct(AttendanceService $attendanceService)
     {
+        $this->attendanceService = $attendanceService;
         $this->middleware('check.booking.user');
     }
     public function update($id)
     {
+        $attendance = $this->attendanceService->update($id) ; 
+        if($attendance){
+            return $this->successResponse('Attendance has been successfully registered.');
+        }
+        else{
+            return $this->errorResponse('Registration was not successful.');
+        }
 
-        $appointment = Appointment::findOrFail($id);
-
-        $attendance = $appointment->attendances->first();
         
-        $attendance->update(['status' => 'present']);
 
-        return $this->errorResponse('Attendance has been successfully registered.');
+    
     }
 
 }
